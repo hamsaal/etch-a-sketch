684bf55d6235df51458338f9898296f16c93a4ab
hamsaal <hs8403340@gmail.com>
2023-11-01 18:58:06 +0100

    Create grid creation function

--- index.js
+++ index.js
@@ -9,13 +9,39 @@ const CreateDiv =()=>{
 }
 // Create function which styles our Grid's rows
 
-const StyleDiv=(div)=>{
-    div.classList.toggle("grid");
+const StyleRow=(div)=>{
+    div.classList.toggle("grid-row");
 }
 
-//Create function which creates grid
-for (let i= 0; i<16;i++){
-    const row = CreateDiv();
-    StyleDiv(row);
-    padContainer.appendChild(row);
+// Create a function which styles our Grid's columns
+const StyleCell =(div)=>{
+    div.classList.toggle("grid-cell");
 }
+
+// Create a function which creates columns for each row
+const CreateColumns =(row)=>{
+    for (let i = 0; i<16; i++){
+        const gridCell = CreateDiv();
+        StyleCell(gridCell);
+        row.appendChild(gridCell);  
+}
+
+}
+
+//Create function which creates a 16X 16 grid 
+
+const CreateGrid =()=>{
+
+    for(let i = 0 ; i< 16 ; i++){
+        const gridRow=CreateDiv();
+        StyleRow(gridRow);
+        padContainer.appendChild(gridRow)
+        CreateColumns(gridRow);
+        
+    }
+
+
+    }
+
+
+CreateGrid();
