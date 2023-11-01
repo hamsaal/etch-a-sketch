eaf94b6e41814a7d96f558bf548b8ecf79abd9ef
hamsaal <hs8403340@gmail.com>
2023-11-01 18:25:49 +0100

    Create rows to accomodate grid cells

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hello world");
+let padContainer = document.querySelector(".sketch-pad");
+
+//  Create functions such that it creates division
+
+const CreateDiv =()=>{
+        let div= document.createElement("div");
+        return div;
+
+}
+// Create function which styles our Grid's rows
+
+const StyleDiv=(div)=>{
+    div.classList.toggle("grid");
+}
+
+//Create function which creates grid
+for (let i= 0; i<16;i++){
+    const row = CreateDiv();
+    StyleDiv(row);
+    padContainer.appendChild(row);
+}
